f4c847fc5f29afd9fc75e28ed225e15ccb36fde5
ruben.sancho <sancho.ruben@gmail.com>
2020-10-01 01:05:15 +0200

    Archivo de rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +10,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      initialRoute: 'home',
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
+      },
     );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icon_string_util.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,27 +20,23 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listItems(snapshot.data),
+          children: _listItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listItems(List<dynamic> data) {
+  List<Widget> _listItems(List<dynamic> data, BuildContext context) {
     final List<Widget> options = [];
 
     data.forEach((element) {
       final tempWidget = ListTile(
         title: Text(element['texto']),
-        leading: Icon(
-          Icons.account_balance_wallet,
-          color: Colors.blue,
-        ),
-        trailing: Icon(
-          Icons.keyboard_arrow_right,
-          color: Colors.blue,
-        ),
-        onTap: () {},
+        leading: getIcon(element['icon']),
+        trailing: getIcon('keyboard_arrow_right'),
+        onTap: () {
+          Navigator.pushNamed(context, element['ruta']);
+        },
       );
 
       options..add(tempWidget)..add(Divider());
