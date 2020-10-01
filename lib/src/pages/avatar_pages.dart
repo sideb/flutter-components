6eb89ccf7237bde8345fa81c352ecd7b19167f50
ruben.sancho <sancho.ruben@gmail.com>
2020-10-01 17:33:36 +0200

    Avatar page

--- lib/src/pages/avatar_pages.dart
+++ lib/src/pages/avatar_pages.dart
@@ -6,6 +6,23 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://aws.revistavanityfair.es/prod/designs/v1/assets/2444x2020/170456.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('RS'),
+              backgroundColor: Colors.green,
+            ),
+          ),
+        ],
       ),
     );
   }
