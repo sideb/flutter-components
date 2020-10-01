d5d26d4ef5e0a7907d73cdb4027869aad03ef91c
ruben.sancho <sancho.ruben@gmail.com>
2020-10-01 17:22:28 +0200

    Cards samples

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,46 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _showAlert(context),
+        ),
+      ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la caja de alerta'),
+              FlutterLogo(
+                size: 100.0,
+              )
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Cancelar')),
+            FlatButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('OK')),
+          ],
+        );
+      },
     );
   }
 }
